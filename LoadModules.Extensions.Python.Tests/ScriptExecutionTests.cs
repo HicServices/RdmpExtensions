eb0cdf098d352b5a4b6041ef92ae8db5563abd67
Thomas Nind <tznind@dundee.ac.uk>
2016-09-21 09:35:24 +0000

    New test that confirms that events flow out of Python async (i.e. not 'all at the end').  This requires flush unfortunately in the Python script... rly? ;-\
    git-svn-id: https://hic.dundee.ac.uk/svn/rdmpextensions@12 e25711a6-e5a0-934c-b3ef-559aeccc148d

--- LoadModules.Extensions.Python.Tests/ScriptExecutionTests.cs
+++ LoadModules.Extensions.Python.Tests/ScriptExecutionTests.cs
@@ -56,5 +56,59 @@ time.sleep(1)
                 file.Delete();
             }
         }
+
+        [Test]
+        public void SlowRollerAsync()
+        {
+            var script =
+@"import time
+import sys
+
+print (""GetReady"")
+sys.stdout.flush()
+
+time.sleep(5)
+print (""FIGHT!"")
+sys.stdout.flush()
+";
+
+            var file = new FileInfo("threadscript.py");
+
+            File.WriteAllText(file.FullName, script);
+            try
+            {
+                var py = new PythonDataProvider();
+                py.FullPathToPythonScriptToRun = file.FullName;
+                py.Version = PythonVersion.Version3;
+
+                var tomemory = new ToMemoryDataLoadJob();
+
+
+                var task = new Task(() => py.Fetch(tomemory, new GracefulCancellationToken()));
+                task.Start();
+
+                //wait 1 second, the first message should have come through but not the second
+                Task.Delay(2000).Wait();
+
+                Assert.IsTrue(tomemory.EventsReceivedBySender[py].Any(m => m.Message.Equals("GetReady")));
+                Assert.IsFalse(tomemory.EventsReceivedBySender[py].Any(m => m.Message.Equals("FIGHT!")));
+                Assert.IsFalse(task.IsCompleted);
+
+                //wait another 6 seconds
+                Task.Delay(6000).Wait();
+                
+                //now both messages should have come through
+                Assert.IsTrue(tomemory.EventsReceivedBySender[py].Any(m => m.Message.Equals("GetReady")));
+                Assert.IsTrue(tomemory.EventsReceivedBySender[py].Any(m => m.Message.Equals("FIGHT!")));
+
+                //should already be finished
+                Assert.IsTrue(task.IsCompleted);
+
+            }
+            finally
+            {
+                file.Delete();
+            }
+        }
     }
 }
