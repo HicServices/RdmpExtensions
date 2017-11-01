21860af4748d73fce4e54d58f4bfe65b1fde2b57
Leandro Tramma <ltramma@dundee.ac.uk>
2017-11-01 12:56:00 +0000

    RDMPDEV-817: Fixed all the incompatible plugin API methods.
    git-svn-id: https://hic.dundee.ac.uk/svn/rdmpextensions@72 e25711a6-e5a0-934c-b3ef-559aeccc148d

---
+++ AutomationPlugins/LoadModules.Extensions.AutomationPluginsUIs/CommandExecution/Proposals/ProposeExecutionWhenTargetIsAutomateExtractionSchedule.cs
@@ -0,0 +1,31 @@
+﻿using CatalogueManager.CommandExecution.Proposals;
+using CatalogueManager.ItemActivation;
+using LoadModules.Extensions.AutomationPlugins.Data;
+using LoadModules.Extensions.AutomationPluginsUIs.Tabs;
+using ReusableLibraryCode.CommandExecution;
+using ReusableUIComponents.CommandExecution;
+
+namespace LoadModules.Extensions.AutomationPluginsUIs.CommandExecution.Proposals
+{
+    public class ProposeExecutionWhenTargetIsAutomateExtractionSchedule : RDMPCommandExecutionProposal<AutomateExtractionSchedule>
+    {
+        public ProposeExecutionWhenTargetIsAutomateExtractionSchedule(IActivateItems itemActivator) : base(itemActivator)
+        {
+        }
+
+        public override bool CanActivate(AutomateExtractionSchedule target)
+        {
+            return true;
+        }
+
+        public override void Activate(AutomateExtractionSchedule target)
+        {
+            ItemActivator.Activate<AutomateExtractionScheduleTab, AutomateExtractionSchedule>(target);
+        }
+
+        public override ICommandExecution ProposeExecution(ICommand cmd, AutomateExtractionSchedule target, InsertOption insertOption = InsertOption.Default)
+        {
+            return null;
+        }
+    }
+}
