2b3ffc10865a135bc3e2653a0a1b5a4a6fdd8e7e
Thomas Nind <tznind@dundee.ac.uk>
2019-05-31 07:12:04 +0000

    Added SharedAssemblyInfo.cs to projects
    git-svn-id: https://hic.dundee.ac.uk/svn/rdmpextensions@153 e25711a6-e5a0-934c-b3ef-559aeccc148d

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
+[assembly: AssemblyProduct("Extensions")]
+[assembly: AssemblyCopyright("Copyright (c) 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
