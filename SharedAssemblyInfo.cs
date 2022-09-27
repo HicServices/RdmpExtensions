fdcad42db323ca3c33b53f69ee373a6f575078ed
tznind <tznind@dundee.ac.uk>
2022-09-27 12:24:34 +0100

    Version 6.0.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,6 +6,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0")]
