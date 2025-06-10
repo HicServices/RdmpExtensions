8702e8155743e5a13f268bfc6a331aa35223d434
James Friel <JFriel001@dundee.ac.uk>
2025-06-10 14:42:11 +0100

    bump rdmp

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("6.2.10")]
-[assembly: AssemblyFileVersion("6.2.10")]
-[assembly: AssemblyInformationalVersion("6.2.10")]
+[assembly: AssemblyVersion("6.2.11")]
+[assembly: AssemblyFileVersion("6.2.11")]
+[assembly: AssemblyInformationalVersion("6.2.11")]
